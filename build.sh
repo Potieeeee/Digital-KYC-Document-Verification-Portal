5bb0edb29d93ad45c816d5e2040b186e402964e8
Potieeeee <gelmay30@gmail.com>
2026-06-03 11:15:09 +0800

    created default user for reviewer

--- build.sh
+++ build.sh
@@ -7,3 +7,27 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py create_roles
+
+DEFAULT_REVIEWER_USERNAME=${DEFAULT_REVIEWER_USERNAME:-reviewer}
+DEFAULT_REVIEWER_EMAIL=${DEFAULT_REVIEWER_EMAIL:-reviewer@example.com}
+DEFAULT_REVIEWER_PASSWORD=${DEFAULT_REVIEWER_PASSWORD:-reviewerpassword}
+
+python manage.py shell <<'PY'
+import os
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+
+User = get_user_model()
+username = os.getenv('DEFAULT_REVIEWER_USERNAME', 'reviewer')
+email = os.getenv('DEFAULT_REVIEWER_EMAIL', 'reviewer@example.com')
+password = os.getenv('DEFAULT_REVIEWER_PASSWORD', 'reviewerpassword')
+
+if User.objects.filter(username=username).exists():
+    print(f'Default reviewer already exists: {username}')
+else:
+    user = User.objects.create_user(username=username, email=email, password=password)
+    reviewer_group, _ = Group.objects.get_or_create(name='Reviewer')
+    user.groups.add(reviewer_group)
+    user.save()
+    print(f'Default reviewer created: {username}')
+PY
